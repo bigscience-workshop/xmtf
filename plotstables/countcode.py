608f159b3020f924d2f4f8ebb7b49bdfc3351f91
Niklas Muennighoff <n.muennighoff@gmail.com>
2022-10-20 19:41:33 +0200

    Update countcode.py

--- plotstables/countcode.py
+++ plotstables/countcode.py
@@ -12,7 +12,7 @@ char_count = sum([len(t) for t in counts["targets"]])
 char_avg = char_count / len(counts)
 
 comment_count = sum(counts["counts"])
-comment_avg = sum(counts["counts"]) / len(counts)
+comment_avg = comment_count / len(counts)
 
 print(f"Char Avg {char_avg} ; Comment Avg {comment_avg}")
 # Char Avg 530.5055696223101 ; Comment Avg 0.8540124473543094
